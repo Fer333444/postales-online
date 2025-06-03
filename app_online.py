71d6a44622730c70bfc24939752686c5926c2505
Fercho002 <burgos00012@gmail.com>
2025-06-03 14:23:29 +0200

    fix: configura host y puerto para Render

--- app_online.py
+++ app_online.py
@@ -51,4 +51,7 @@ def servir_postal(archivo):
 
 # Render lo ejecuta
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
